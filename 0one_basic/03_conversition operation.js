caa2593bfd41ed3825e7b83cee9f8b60c5a5c3f8
Manu7767 <khanmanu996@gmail.com>
2025-03-24 11:43:20 +0000

    convetsitons of datatypes

---
+++ 0one_basic/03_conversition operation.js
@@ -0,0 +1,17 @@
+let score = "33"
+
+// console.log(typeof score);
+
+let valuinNumber = Number(score)
+// console.log(typeof valuinNumber);
+
+// one => true ; 0 => false
+// ""=> false;
+// "Manu" => true
+
+let Somenumber = 33
+
+let stringnumber = String(Somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
