a651375e3527f4e5e30a0fdf2ebdfb5e73ec0bd3
Manu7767 <khanmanu996@gmail.com>
2025-03-24 12:13:26 +0000

    conversiotion-2

--- 0one_basic/03_conversition operation.js
+++ 0one_basic/03_conversition operation.js
@@ -12,6 +12,37 @@ let valuinNumber = Number(score)
 let Somenumber = 33
 
 let stringnumber = String(Somenumber)
-console.log(stringnumber);
-console.log(typeof stringnumber);
+// console.log(stringnumber);
+// console.log(typeof stringnumber);
+
+// ------------------- Operations ------------
+
+let value = 3
+
+let negvalue = -value
+// console.log(negvalue);
+
+let strone = "hello"
+let str2 = " Manu"
+let str3 = strone + str2
+// console.log(str3);
+
+// console.log("2" + 3);
+// console.log(3 + "4");
+// console.log("4"+ 5 + 4);
+// console.log(3 + 4 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let Gamecounter = 200
+Gamecounter++;
+console.log(Gamecounter);
+
+
+
+
+
+
+
 
