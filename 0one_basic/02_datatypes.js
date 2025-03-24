6c1ec9bbd29aa368abd9cb3e50aa7e727d17cc50
Manu7767 <khanmanu996@gmail.com>
2025-03-24 11:19:07 +0000

    datatypes

---
+++ 0one_basic/02_datatypes.js
@@ -0,0 +1,15 @@
+"use srtict" // treat all JS code as a newer version.
+
+// alert(3+3) we are using node Js not browser
+
+// data types --
+/*
+Number
+String
+boolian
+null
+undifiend
+bigint
+symbol
+
+*/
