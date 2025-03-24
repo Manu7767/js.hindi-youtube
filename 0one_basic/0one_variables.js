f144028f064a3a990544108c14acb67bdde1f6f5
Manu7767 <khanmanu996@gmail.com>
2025-03-24 07:14:42 +0000

    learn variablesl

---
+++ 0one_basic/0one_variables.js
@@ -0,0 +1,13 @@
+const accoundId = 445533
+let accountemail = "khanmanu@gmail.com"
+var accountPassword = "786786"
+accountCity = "Jaipur"
+
+// accoundId = 55 // not allowed
+console.log(accoundId);
+
+accountemail = "manukhan@343"
+accountPassword = "785789"
+accountCity = "Bengaluru"
+
+console.table([accoundId , accountPassword , accountCity]);
