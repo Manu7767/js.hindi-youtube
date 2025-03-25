1867bf7f207e0dce21fd2da876a37826efeb8bd8
Manu7767 <khanmanu996@gmail.com>
2025-03-25 06:25:20 +0000

    Strings

---
+++ 0one_basic/05_string.js
@@ -0,0 +1,32 @@
+const name = "Manu"
+const repocount = 50
+
+// console.log(name + repocount + "value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+
+// const gamename = new String('manukh')
+// // console.log(gamename[0]);
+// // console.log(gamename.charAt(2));
+// console.log(gamename.indexOf('k'));
+
+// const newstring = gamename.substring(0 , 4);
+
+// console.log(newstring);
+
+// const anothertstring = gamename.slice(-6 , 2)
+
+// console.log(anothertstring);
+
+const newstringone = "   manu   "
+
+console.log(newstringone);
+console.log(newstringone.trim());
+
+const url = "http://Manu.com/Manu%20Khan"
+console.log(url.replace('%20' , "_"));
+console.log(url.includes('radhe'));
+console.log(url.includes('Khan'));
+
+
+
