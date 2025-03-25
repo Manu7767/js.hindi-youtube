d8a39b371b94795238b9353728f7c2a065705e97
Manu7767 <khanmanu996@gmail.com>
2025-03-25 05:42:29 +0000

    stack and heap

---
+++ 0one_basic/datatypes_summary.js
@@ -0,0 +1,34 @@
+// Primitive data types---
+/*
+7 Types: number , string , boolean , null , undefiend , symbol , bigint.
+
+Reference (Non primitive): Arrays , Objects , Functions
+
+*/
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// 2 Types of memories--
+
+// Stack(Premetive-datatypes me use hoti h) , Heap(non-premetive datatypes me);
+
+let myyoutubename = "Manukhandotcom"
+let anothername = myyoutubename
+
+anothername = "chaiaurcode"
+
+console.log(myyoutubename);
+console.log(anothername);
+
+let userOne = {
+    email:  "uset@google.uzy",
+      upi:  "user@ksdji",
+
+}
+
+let usertwo = userOne
+
+usertwo.email = "khan@manu"
+
+console.log(userOne.email);
+console.log(usertwo.email);
