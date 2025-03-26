8b3b486145f0d986e2f46944aaffe5cde3d32cf0
Manu7767 <khanmanu996@gmail.com>
2025-03-26 05:01:28 +0000

    Start Arrays

---
+++ 02_basics/0one_arrays.js
@@ -0,0 +1,39 @@
+// Arrays
+
+
+
+// const myHeros = ["Ironman" , "Thor" , "Hulk"]
+
+// const myArr2 = new Array(2 , 3 , 4 )
+// console.log(myArr[2]);
+// console.log(myHeros);
+
+// Array Methods
+
+// myArr.push(9);
+// console.log(myArr);
+
+// myArr.pop()
+// myArr.unshift(9)
+// myArr.shift()
+
+
+
+// const newArr = myArr.join()
+// console.log(myArr);
+// console.log(  typeof newArr);
+
+
+// Slice , Splice
+
+const myArr = [2 , 3 , 4 ,5 , 6 ,7 ,8];
+
+console.log("A " , myArr);
+const mynone = myArr.slice(2, 4);
+console.log(mynone);
+console.log("B" , myArr);
+
+
+
+
+ 
