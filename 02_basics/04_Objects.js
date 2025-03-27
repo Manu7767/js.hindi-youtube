0a5b3977aed3e4f2e4c928c74840e4cfc690aeb0
Manu7767 <khanmanu996@gmail.com>
2025-03-27 06:47:39 +0000

    Objects depts

---
+++ 02_basics/04_Objects.js
@@ -0,0 +1,42 @@
+// Singleton Object's
+
+const tinderuser = new Object()
+
+tinderuser.id = "2323"
+tinderuser.name = "Sam"
+tinderuser.isloggedIn = true
+
+// console.log(tinderuser);
+
+const regularuser = {
+    email: "some@342343.com",
+    fullname: {
+        userfullname: {
+            firstName: "Manu",
+            LastName: "Khan"
+        }
+    }
+}
+
+// console.log(regularuser.fullname);
+
+const obj = {2 : "a", 3: "b" }
+const obj2 = {4: "c", 5: "d"}
+
+// const obj3 = Object.assign({}, obj , obj2);
+
+const obj3 = {...obj , ...obj2}
+
+// console.log(obj3);
+
+console.log(tinderuser);
+console.log(Object.keys(tinderuser));
+console.log(Object.values(tinderuser));
+console.log(Object.entries(tinderuser))
+
+
+console.log(tinderuser.hasOwnProperty('isloggedIn'))
+
+
+
+
