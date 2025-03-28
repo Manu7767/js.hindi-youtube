71d2bfee2c060437f55112be974ebd3df44aefdc
Manu7767 <khanmanu996@gmail.com>
2025-03-28 06:04:51 +0000

    Arrows and Scops

---
+++ 03_basics/02_scops.js
@@ -0,0 +1,39 @@
+// function one(){
+//     const username = "Manu"
+
+//     function two(){
+//         const website = "youtube"
+//         console.log(username);
+
+//     }
+
+//     console.log(website);
+
+//     two()
+
+// }
+
+// one()
+
+// if (true) {
+//     const username = "Manu"
+//     if (username === "Manu") {
+
+//         const website = " Youtube"
+
+//         console.log(username + website);
+
+//     }
+// }
+
+// +++++++++++++++++++++ interesting +++++++++++++++
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+const addtwo = function(num){
+    return num + 2
+}
+addtwo()
---
+++ 03_basics/03_Arrow.js
@@ -0,0 +1,37 @@
+const user = {
+    username: "manu" ,
+    price: 999,
+
+    WelcomeMessage: function(){
+        // console.log(`${this.username} , Welcome to website`);
+        
+    }
+}
+user.username = "Sam"
+user.WelcomeMessage()
+// user.WelcomeMessage()
+
+const chai =  ()=>{
+
+    let username = "Manu"
+    // console.log(this.username);
+    
+
+}
+// chai()
+
+// const addtwo = (num, num2)=>{
+
+//     return num + num2
+
+// }   
+// console.log(addtwo(4,5))
+
+// const addtwo = (num, num2)=> num + num2
+// console.log(addtwo(4,5))
+
+const addtwo = (num, num2)=> (num + num2)     // {},me return ka use hoga , (),me return ka use nahi hogaa..
+console.log(addtwo(4,5))
+
+   
+
