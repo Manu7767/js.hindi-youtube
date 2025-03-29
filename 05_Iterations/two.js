9c22a4606ee408b9eb5d36858c9b2c0f9e1261ec
Manu7767 <khanmanu996@gmail.com>
2025-03-29 06:18:37 +0000

    do-while loops

---
+++ 05_Iterations/two.js
@@ -0,0 +1,24 @@
+// ---------- While and Do-While Loop --------
+
+// let index = 0
+
+// while (index<=9) {
+
+//     console.log(`Value of index is ${index}`);
+//     index = index+2
+// }
+    
+// let myArray = ["Ironman", "Thor", "Hulk"]
+// let arr = 0
+// while (arr<myArray.length) {
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr = arr+1
+// }
+    
+let score = 12
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+    
+} while (score<=10);
